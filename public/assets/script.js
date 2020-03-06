f48395da9266b8b81c4e2baef11a3cf82c3b497e
Walid <walid.benkhelfallah@ynov.com>
2020-03-06 12:27:51 +0100

    scriptconsole

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,2 +1,16 @@
-const paragapheElement = document.querySelector('user_name');
-console.dir('user_name')
+
+
+ function passvalues(){
+    var name = document.getElementById("name");
+    var city = name.value;
+
+ 
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=230487580a6de8abdd8475c5c8a22fb9`
+    
+  )
+  .then(response => response.json())
+  .then(data => {
+      console.log(data);
+  });
+ };
