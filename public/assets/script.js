bd0538deb6fc7867ff2de8f2c60f4488f5b216f8
walid1999 <56828930+walid1999@users.noreply.github.com>
2020-03-07 19:23:11 +0100

    ProjetJavascript

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,15 +1,9 @@
 
- 
-// Exécute un appel AJAX GET
-// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
-
-
 function ajaxGet(url, callback) {
   const req = new XMLHttpRequest();
   req.open("GET", url);
   req.addEventListener("load", function () {
       if (req.status >= 200 && req.status < 400) {
-          // Appelle la fonction callback en lui passant la réponse de la requête
           callback(req.responseText);
       } else {
           console.error(req.status + " " + req.statusText + " " + url);
@@ -26,29 +20,29 @@ function afficher(reponse){
 }
 
 function passvalues(){
+
   const name = document.getElementById("name");
   const city = name.value;
-
-
-
-
 
 ajaxGet(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=230487580a6de8abdd8475c5c8a22fb9`, function(reponse) {
 
   const meteo = JSON.parse(reponse);
-  // Récupération de certains résultats
+  
+
   const temperature = meteo.main.temp;
+ 
 
   temperatureR = Math.round(temperature);
   
 
-  // Affichage des résultats
+  
   const conditionsElt = document.createElement("div");
   conditionsElt.textContent = `Il fait actuellement ` + (temperatureR-273)+` °C à ` + city ;
+
+  
   
   const meteoElt = document.getElementById("meteo");
   meteoElt.appendChild(conditionsElt);
-  
 
 });
 };
